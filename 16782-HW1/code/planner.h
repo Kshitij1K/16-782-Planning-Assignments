20804e00dcdd17cb7838073c445d5a818a7287dc
Kshitij Kabeer <kshitijkabeer@gmail.com>
2023-09-12 17:51:36 -0400

    Add HW1 files

---
+++ 16782-HW1/code/planner.h
@@ -0,0 +1,20 @@
+#ifndef PLANNER_H
+#define PLANNER_H
+
+// Declare the plan function
+void planner(
+    double*	map,
+    int collision_thresh,
+    int x_size,
+    int y_size,
+    int robotposeX,
+    int robotposeY,
+    int target_steps,
+    double* target_traj,
+    int targetposeX,
+    int targetposeY,
+    int curr_time,
+    double* action_ptr
+    );
+
+#endif // PLANNER_H
