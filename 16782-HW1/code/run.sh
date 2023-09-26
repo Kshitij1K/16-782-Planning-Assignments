ddfdd47b5180670c6cf2ceff662717194153d2b6
Kshitij Kabeer <kshitijkabeer@gmail.com>
2023-09-26 01:56:23 -0400

    Add visualization scripts

---
+++ 16782-HW1/code/run.sh
@@ -0,0 +1,42 @@
+./a.out map9.txt
+python visualizer_only_target.py map9.txt 9
+python visualizer_only_robot.py map9.txt 9
+python visualizer_both.py map9.txt 9
+
+./a.out map8.txt
+python visualizer_only_target.py map8.txt 8
+python visualizer_only_robot.py map8.txt 8
+python visualizer_both.py map8.txt 8
+
+./a.out map7.txt
+python visualizer_only_target.py map7.txt 7
+python visualizer_only_robot.py map7.txt 7
+python visualizer_both.py map7.txt 7
+
+./a.out map6.txt
+python visualizer_only_target.py map6.txt 6
+python visualizer_only_robot.py map6.txt 6
+python visualizer_both.py map6.txt 6
+
+./a.out map5.txt
+python visualizer_only_target.py map5.txt 5
+python visualizer_only_robot.py map5.txt 5
+python visualizer_both.py map5.txt 5
+
+./a.out map4.txt
+python visualizer_only_target.py map4.txt 4
+python visualizer_only_robot.py map4.txt 4
+
+./a.out map3.txt
+python visualizer_only_target.py map3.txt 3
+python visualizer_only_robot.py map3.txt 3
+
+./a.out map2.txt
+python visualizer_only_target.py map2.txt 2
+python visualizer_only_robot.py map2.txt 2
+python visualizer_both.py map2.txt 2
+
+./a.out map1.txt
+python visualizer_only_target.py map1.txt 1
+python visualizer_only_robot.py map1.txt 1
+python visualizer_both.py map1.txt 1
---
+++ 16782-HW1/code/visualizer_both.py
@@ -0,0 +1,86 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib.animation import FuncAnimation
+
+import sys
+
+def parse_mapfile(filename):
+    with open(filename, 'r') as file:
+        assert file.readline().strip() == 'N', "Expected 'N' in the first line"
+        x_size, y_size = map(int, file.readline().strip().split(','))
+        
+        assert file.readline().strip() == 'C', "Expected 'C' in the third line"
+        collision_threshold = int(file.readline().strip())
+        
+        assert file.readline().strip() == 'R', "Expected 'R' in the fifth line"
+        robotX, robotY = map(int, file.readline().strip().split(','))
+
+        assert file.readline().strip() == 'T', "Expected 'T' in the seventh line"
+        target_trajectory = []
+        line = file.readline().strip()
+        while line != 'M':
+            x, y = map(float, line.split(','))
+            target_trajectory.append({'x': x, 'y': y})
+            line = file.readline().strip()
+        
+        costmap = []
+        for line in file:
+            row = list(map(float, line.strip().split(',')))
+            costmap.append(row)
+        
+        costmap = np.asarray(costmap).T
+    
+    return x_size, y_size, collision_threshold, robotX, robotY, target_trajectory, costmap
+
+def parse_robot_trajectory_file(filename):
+    robot_trajectory = []
+    with open(filename, 'r') as file:
+        for line in file:
+            t, x, y = map(int, line.strip().split(','))
+            robot_trajectory.append({'t': t, 'x': x, 'y': y})
+    
+    return robot_trajectory
+
+SPEEDUP = 200000
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python visualizer.py <map filename> <map number>")
+        sys.exit(1)
+    
+    x_size, y_size, collision_threshold, robotX, robotY, target_trajectory, costmap = parse_mapfile(sys.argv[1])
+
+    robot_trajectory = parse_robot_trajectory_file('robot_trajectory.txt')
+
+    fig, ax = plt.subplots()
+    
+    ax.imshow(costmap)
+    
+    line1, = ax.plot([], [], lw=2, marker='o', color='b', label='robot')
+    line2, = ax.plot([], [], lw=2, marker='o', color='r', label='target')
+    line1.set_data([], [])
+    line2.set_data([], [])
+    line1.set_data([p['x'] for p in robot_trajectory], [p['y'] for p in robot_trajectory])
+    line2.set_data([p['x'] for p in target_trajectory], [p['y'] for p in target_trajectory])
+    plt.savefig('map_images/map' + str(sys.argv[2]) + '_both.png')
+    
+    # def init():
+        # return line1, line2
+        # return line1
+        # return line2
+    
+    # def update(frame):
+        # line1.set_data([p['x'] for p in robot_trajectory[:frame+1]], [p['y'] for p in robot_trajectory[:frame+1]])
+        
+        # t = robot_trajectory[frame+1]['t']
+        
+        # plt.pause((robot_trajectory[frame+1]['t']-robot_trajectory[frame]['t'])/SPEEDUP)
+
+        # return line1, line2
+        # return line1
+        # return line2
+    
+    # ani = FuncAnimation(fig, update, frames=1, init_func=init, blit=False, interval=1)
+
+    plt.legend()
+    # plt.show()
